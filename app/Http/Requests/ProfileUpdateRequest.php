934d35a35b08566db720ecc120ec80b060e61e31
ms-Maryna <d00283071@student.dkit.ie>
2026-04-26 20:35:08 +0100

    add avatar validation rule to profile update request

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -16,16 +16,17 @@ class ProfileUpdateRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => [
-                'required',
-                'string',
-                'lowercase',
-                'email',
-                'max:255',
-                Rule::unique(User::class)->ignore($this->user()->id),
-            ],
-        ];
+       return [
+        'name' => ['required', 'string', 'max:255'],
+        'email' => [
+        'required',
+        'string',
+        'lowercase',
+        'email',
+        'max:255',
+        Rule::unique(User::class)->ignore($this->user()->id),
+    ],
+    'avatar' => ['nullable', 'image', 'mimes:jpg,jpeg,png,gif', 'max:2048'],
+];
     }
 }
